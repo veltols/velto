bf9ca571ed437a12e3987458afc26b4123dcaaae
mohsanch <mohsanchaudhary11@gmail.com>
2026-01-30 07:28:33 +0500

    no index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
 <head>
     <meta charset="utf-8">
+    <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@hasSection('title') @yield('title') - {{ config('app.name', 'Velto') }} @else {{ config('app.name', 'Velto') }} @endif</title>
