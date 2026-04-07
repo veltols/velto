317a7c6c85b0fc412d94a8ba60a0c40d64120f7e
mohsanch <mohsanchaudhary11@gmail.com>
2026-04-07 21:42:58 +0500

    env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
 <head>
     <meta charset="utf-8">
-    <meta name="robots" content="noindex">
+    @if(config('app.env') !== 'production')
+        <meta name="robots" content="noindex">
+    @endif
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@hasSection('title') @yield('title') - {{ config('app.name', 'Velto') }} @else {{ config('app.name', 'Velto') }} @endif</title>
