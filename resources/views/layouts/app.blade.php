1c42c77697c7592db35f5e67e4eef153dad0f943
mohsanch <mohsanchaudhary11@gmail.com>
2026-07-18 01:19:07 +0500

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;0,700;1,400&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
     
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="{{ asset('images/headerlogo.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.webp') }}">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
