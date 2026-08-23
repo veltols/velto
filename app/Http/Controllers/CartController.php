e1967fad4d26c7788359384679f7c861c21d8060
mohsanch <mohsanchaudhary11@gmail.com>
2026-08-24 00:22:37 +0500

    admin side responsive

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cart;
-use App\Models\Product;
+use App\Models\Product; 
 use App\Models\ProductVariant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
